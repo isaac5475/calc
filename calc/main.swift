b987003db3d5ed26116369ae3893397aebe59601
Murat <murat.zaydullin@student.uts.edu.au>
2025-03-06 17:59:34 +1100

    parse and create Operation() (only supports +/-)

--- calc/main.swift
+++ calc/main.swift
@@ -10,8 +10,11 @@ import Foundation
 
 var args = ProcessInfo.processInfo.arguments
 args.removeFirst() // remove the name of the program
-let const5 = Operation(constant: 5);
-let const10 = Operation(constant: 10);
+do {
+    let op = try parse(args);
+    print("args: ", args);
+    print("Operation: ", op);
+    print("=", op.compute());
+} catch ParseError.IllegalArguments {
 
-let add = Operation(operand1: const5, operand2: const10, operationType: OpType.ADD)
-print("10+5=", add.compute())
+}
---
+++ calc/parse.swift
@@ -0,0 +1,61 @@
+//
+//  parse.swift
+//  calc
+//
+//  Created by Murat Zaydullin on 6/3/2025.
+//  Copyright © 2025 UTS. All rights reserved.
+//
+enum ParseError : Error {
+    case IllegalArguments
+}
+func parse(_ args: [String]) throws -> Operation {
+    if !isValidInput(args) {
+        throw ParseError.IllegalArguments;
+    }
+    var argsM = args;
+    var result =  Operation(constant: Double(argsM.popLast() ?? "0")!);
+    
+    while !argsM.isEmpty {
+        let operationType = switch argsM.popLast() ?? "" {
+        case "+":
+            OpType.ADD
+        case "-":
+            OpType.SUBTR
+        default:
+            OpType.CONST
+        }
+        if operationType == OpType.CONST {
+            throw ParseError.IllegalArguments
+        }
+        let val = Double(argsM.popLast()!)!
+        result = Operation(operand1: Operation(constant: val), operand2: result, operationType: operationType)
+    }
+    return result;
+}
+
+func isValidInput(_ args: [String]) -> Bool {
+    if args.isEmpty || args.count < 3 {
+        return false;
+    }
+    let operators = [
+        "+",
+        "-",
+        "x",
+        "/",
+        "%"
+    ];
+    var isNextOperator = false    // alternates between digits and operators
+    for token in args {
+        if isNextOperator {
+            if !operators.contains(token) {
+                return false;
+            }
+        } else {
+            if Double(token) == nil {
+                return false;
+            }
+        }
+        isNextOperator = !isNextOperator;
+    }
+    return true;
+}
